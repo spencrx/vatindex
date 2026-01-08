2a21349b4da98490a392ed7dfb90b96e570ffeb1
spencrx <spencer.blax@gmail.com>
2026-01-08 13:55:56 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,13 @@
 import { boho } from "@/lib/boho";
 
 export default boho.middleware;
+
+export const config = {
+  matcher: [
+    // Run middleware on everything EXCEPT:
+    // - /api routes
+    // - next static/image assets
+    // - favicon
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
